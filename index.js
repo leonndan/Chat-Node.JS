dfe3a58fd8040e317f541aa2e9dee77c9030d21b
Daniel León <97914817+leonndan@users.noreply.github.com>
2022-03-05 18:13:01 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const socket = require("socket.io");
+const port= process.env.PORT||4000;
+
+// App setup
+var app = express();
+var server = app.listen(port, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
